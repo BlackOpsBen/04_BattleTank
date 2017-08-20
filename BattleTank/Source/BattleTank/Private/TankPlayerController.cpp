3796bda181fb060fb30bfbbcf9fbf6f6c3230966
Ben Mora <blackopsben@gmail.com>
2017-08-19 23:42:40 -0500

    Ready for 129

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -53,6 +53,14 @@ void ATankPlayerController::AimTowardsCrosshair()
 //Get world location of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {
-	HitLocation = FVector(1.0f);
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+	HitLocation.X = ScreenLocation.X;
+	HitLocation.Y = ScreenLocation.Y;
+
+	// De-project the screen position of the crosshair to a world direction
+	// Line-trace along that look direction and see what we hit (up to max range)
 	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,14 +15,19 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	virtual void Tick(float DeltaTime) override;
 
 	void BeginPlay() override;
 
 	ATank* GetControlledTank() const;
 
-private:
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333;
+
 	// Start moving the barrel so that the shot would hit where the crosshair hits the world.
 	void AimTowardsCrosshair();
 
